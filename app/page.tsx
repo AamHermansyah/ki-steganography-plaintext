72253619e504354519428379fcc68e478cd6ecc8
Aam Hermansyah <amzhermanzyah@gmail.com>
2024-05-10 22:25:25 +0700

    [UPDATE] Fix title size in mobile

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <>
       <main className="min-h-screen w-full max-w-7xl flex flex-col items-center justify-center mx-auto gap-6 sm:gap-10 px-4 sm:px-10 pt-6 pb-10 sm:py-10">
-        <h1 className="scroll-m-20 text-4xl tracking-tight text-primary font-bold uppercase text-center">
+        <h1 className="scroll-m-20 text-3xl sm:text-4xl tracking-tight text-primary font-bold uppercase text-center">
           Steganografi
           <br />
           Pesan Rahasia Dalam Teks Biasa
